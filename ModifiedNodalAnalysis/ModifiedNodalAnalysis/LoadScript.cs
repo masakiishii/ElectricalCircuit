0a30e457e244f9edb1b0af81b6f126b0941505aa
masakiishii <masaki.ishii511@gmail.com>
2014-11-29 16:21:31 +0900

    add Program.cs, LoadScript.cs and test_circuit_1.txt

---
+++ ModifiedNodalAnalysis/ModifiedNodalAnalysis/LoadScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifiedNodalAnalysis
+{
+    class LoadScript
+    {
+        private string filename;
+        public LoadScript(string filename)
+        {
+            this.filename = filename;
+        }
+        public void parseCommandLine()
+        {
+            string line   = string.Empty;
+            string result = string.Empty;
+            if (System.IO.File.Exists(this.filename))
+            {
+                System.IO.StreamReader reader = (new System.IO.StreamReader(this.filename, System.Text.Encoding.Default));
+                while(reader.Peek() >= 0)
+                {
+                    line = reader.ReadLine();
+                    Console.WriteLine(line);
+                }
+                reader.Close();
+            }
+        }
+    }
+}
---
+++ ModifiedNodalAnalysis/ModifiedNodalAnalysis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifiedNodalAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                LoadScript loadscript = new LoadScript(args[0]);
+                loadscript.parseCommandLine();
+            } else
+            {
+                Console.WriteLine("Error!");  
+            }
+            Console.ReadLine();
+        }
+    }
+}
