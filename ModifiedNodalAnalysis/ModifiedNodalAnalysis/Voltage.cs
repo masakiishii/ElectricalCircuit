4af8a64c520e608e43f8013773ca0788a1febf94
masakiishii <masaki.ishii511@gmail.com>
2014-12-20 22:55:52 +0900

    add Voltage class derived from Element base class

---
+++ ModifiedNodalAnalysis/ModifiedNodalAnalysis/Voltage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifiedNodalAnalysis
+{
+    class Voltage : Element
+    {
+        public Voltage(int value, int prenode, int postnode) : base(value, prenode, postnode) { }
+        
+        public override void getType() {
+            Console.WriteLine("Element: Voltage");
+        }
+
+    }
+}
