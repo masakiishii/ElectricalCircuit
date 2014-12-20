4e500f05be68bfc59b03adbb2aa48614ffa708ee
masakiishii <masaki.ishii511@gmail.com>
2014-12-20 23:27:20 +0900

    add MatrixBuilder class

---
+++ ModifiedNodalAnalysis/ModifiedNodalAnalysis/MatrixBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifiedNodalAnalysis
+{
+    class MatrixBuilder
+    {
+        private Element[]      elementlist;
+        private List<string[]> rawlist;
+        public MatrixBuilder(List<string[]> rawlist)
+        {
+            this.rawlist = rawlist;
+        }
+
+        public void build()
+        {
+            
+        }
+    }
+}
