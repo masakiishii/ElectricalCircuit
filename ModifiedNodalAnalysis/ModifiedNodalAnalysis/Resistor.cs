a5f6a7f3185de65930709e8236f2ccb79e1a3cf6
masakiishii <masaki.ishii511@gmail.com>
2014-12-20 22:52:12 +0900

    add Resistor.cs derived from Element base class

---
+++ ModifiedNodalAnalysis/ModifiedNodalAnalysis/Resistor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifiedNodalAnalysis
+{
+    class Resistor : Element
+    {
+        public Resistor(int value, int prenode, int postnode) : Element(value, prenode, postnode) {}
+        
+        public override void getType() {
+            Console.WriteLine("Element: Resistor");
+        }
+    }
+}
