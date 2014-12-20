d99bb263ac70b1277f7263145873afcbade37b7f
masakiishii <masaki.ishii511@gmail.com>
2014-12-20 22:40:02 +0900

    add Element.cs about abstract class

---
+++ ModifiedNodalAnalysis/ModifiedNodalAnalysis/Element.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifiedNodalAnalysis
+{
+    abstract class Element
+    {
+        protected int value;
+        protected int prenode;
+        protected int postnode;
+
+        public Element(int value, int prenode, int postnode)
+        {
+            this.value    = value;
+            this.prenode  = prenode;
+            this.postnode = postnode;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+
+        public int getPreNode()
+        {
+            return this.prenode;
+        }
+
+        public int getPostNode()
+        {
+            return this.postnode;
+        }
+    }
+}
