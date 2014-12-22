f2ae2b351994106c4d73f724cbe79d828e62d34c
masakiishii <masaki.ishii511@gmail.com>
2014-12-22 18:00:02 +0900

    add CurrentSource.cs

---
+++ ModifiedNodalAnalysis/ModifiedNodalAnalysis/CurrentSource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifiedNodalAnalysis
+{
+    class CurrentSource : Element
+    {
+        private static int count = 0;
+        private int id;
+
+        public CurrentSource(string[] rawline) : base(rawline) {
+            count++;
+            this.id = count;
+        }
+
+        public override void getType() {
+            Console.WriteLine("Element: Current Source");
+        }
+
+        public int getCurrentSourceId()
+        {
+            return this.id;
+        }
+
+        public override void stampElementData(double[,] matrix, double[,] vector, int matrixgsize)
+        {
+            vector[this.id - 1, 0] = this.value;
+        }
+    }
+}
